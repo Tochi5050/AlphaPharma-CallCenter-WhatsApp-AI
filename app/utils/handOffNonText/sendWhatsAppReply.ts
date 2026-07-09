b2796b98ba953c9b0e6f3d875e20d36f7ce6dac3
Tochi5050 <ctochi46@gmail.com>
2026-07-09 14:54:19 +0100

    Fix: Testing 360 dialog api keys

--- app/utils/handOffNonText/sendWhatsAppReply.ts
+++ app/utils/handOffNonText/sendWhatsAppReply.ts
@@ -8,7 +8,7 @@ export async function sendWhatsAppReply(
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "D360-API-KEY": process.env.D360_API_KEY!,
+      "D360-API-KEY": process.env.D460_API_KEYS!,
     },
     body: JSON.stringify({
       messaging_product: "whatsapp",
