f1f3ed2689e28e759affab341364f31c370af2aa
Tochi5050 <ctochi46@gmail.com>
2026-07-13 11:43:53 +0100

    Updated payment details

--- app/utils/paymentDetails/getPaymentDetailsText.ts
+++ app/utils/paymentDetails/getPaymentDetailsText.ts
@@ -1,7 +1,7 @@
 export function getPaymentDetailsText(): string {
   const accountName =
     process.env.PAYMENT_ACCOUNT_NAME || "Alpha Pharmacy and Stores";
-  const accountNumber = process.env.PAYMENT_ACCOUNT_NUMBER || "12345";
-  const bankName = process.env.PAYMENT_BANK_NAME || "Alpha Bank";
+  const accountNumber = process.env.PAYMENT_ACCOUNT_NUMBER || "1010891325";
+  const bankName = process.env.PAYMENT_BANK_NAME || "Zenith Bank";
   return `Acc Name: ${accountName}\nAcc No: ${accountNumber}\nBank: ${bankName}`;
 }
